e3ebcd3678e45ea821ae606189823e8171224893
David <Rigby335@hotmail.com>
2022-08-15 15:46:24 -0700

    added test for list of all users red

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   firstName: 'Test',
@@ -10,9 +11,22 @@ const mockUser = {
   password: '12345',
 };
 
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
   });
   it('creates a new user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
@@ -26,10 +40,13 @@ describe('backend-express-template routes', () => {
   it('signs in an existing user', async () => {
     await request(app).post('/api/v1/users').send(mockUser);
     const res = await request(app).post('/api/v1/users/sessions').send({ email: 'test@test.com', password: '12345' });
-    console.log('res', res.body);
     expect(res.status).toEqual(200);
   });
-  afterAll(() => {
-    pool.end();
+  
+  it('shows a list of all users', async () => {
+    const [agent, user] = await registerAndLogin({ email: 'admin' });
+    const res = await agent.get('/api/v1/users');
+
+    expect(res.body).toEqual([{ ...user }]);
   });
 });
