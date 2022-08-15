30ac6e7f369fe326a1ae8b6a175b2e9c7bdadd3c
David <Rigby335@hotmail.com>
2022-08-15 11:59:00 -0700

    added User model, route, and UserService. changed table name. added cookie-parser to app.js. create user test green.

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,9 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS yawp_users;
 
-CREATE TABLE users (
+CREATE TABLE yawp_users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
