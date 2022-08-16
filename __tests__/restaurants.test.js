9eef0d481915ee8f5cff518a8ab31d49fac1b6c5
David <Rigby335@hotmail.com>
2022-08-16 11:32:27 -0700

    added route for restaurants in controllers, added Restaurant model, list of restuarants test passes green

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -7,11 +7,12 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('should show a list of restaurants', async () => {
-    const res = await request(app).get('/api/v1/restaurants');
-    expect(res.body.length).toEqual(4);
-  });
   afterAll(() => {
     pool.end();
+  });
+  it('should show a list of restaurants', async () => {
+    const res = await request(app).get('/api/v1/restaurants');
+    expect(res.status).toBe(200);
+    expect(res.body.length).toEqual(4);
   });
 });
