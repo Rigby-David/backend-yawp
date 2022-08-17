235eb65744e2c5ec75167fea14b3def3b00e3132
David <Rigby335@hotmail.com>
2022-08-17 08:49:01 -0700

    refactored DELETE test for review w/ matching user, red

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -3,6 +3,13 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const mockUsers = {
+  firstName: 'John',
+  lastName: 'Cena',
+  email: 'test@cena.com',
+  password: '12345',
+};
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -13,18 +20,25 @@ describe('backend-express-template routes', () => {
 
   it('#DELETE should return 401 for unauth users', async () => {
     const res = await request(app).delete('/api/v1/reviews/1');
-    console.log('res.body', res.body);
     expect(res.status).toBe(401);
   });
-  it('#DELETE should return status 200 for user with the same user_id', async () => {
+  it('#DELETE should delete review for user w/ matching user_id', async () => {
+    const newReview = {
+      stars: '3',
+      detail: 'The food was affordable',
+    };
     const agent = request.agent(app);
+    await agent.post('/api/v1/users').send(mockUsers);
 
-    await agent.post('/api/v1/users/sessions').send({
-      email: 'test@example.com',
-      password: '123456',
-    });
+    const response = await agent
+      .post('/api/v1/restaurants/2/reviews')
+      .send(newReview);
+    expect(response.status).toBe(200);
 
-    const res = await agent.delete('/api/v1/reviews/1');
+    const res = await agent.delete('/api/v1/reviews/3');
     expect(res.status).toBe(200);
+
+    // const resp = await agent.get('/api/v1/reviews/3');
+    // expect(resp.status).toBe(404);
   });
 });
